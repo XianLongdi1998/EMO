249179f9ea5bd83cbffa0590588fa8880ae988eb
Xian Longdi <117279132+XianLongdi1998@users.noreply.github.com>
2025-01-02 14:29:13 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,33 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			const role = uni.getStorageSync('role');
+			  if (role !== 1) {
+						console.log('aa')
+						uni.setTabBarItem({
+							index:2,
+							visible:false
+						})
+			  }
+		},
+		onShow: function() {
+			console.log('App Show')
+			const role = uni.getStorageSync('role');
+			  if (role !== 1) {
+						console.log('aa')
+						uni.setTabBarItem({
+							index:2,
+							visible:false
+						})
+			  }
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
